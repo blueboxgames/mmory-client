a8ab00e26575062839e4cfc43126b652dc0f6a3a
Mansour <mansurjavadi@gmail.com>
2019-05-07 15:22:32 +0430

    clear data uncomment commands

--- bats/ClearData.bat
+++ bats/ClearData.bat
@@ -1,24 +1,21 @@
 @echo off
 
-:: Set working dir
-cd %~dp0 & cd ../src
+call bats/SetupApp.bat
 
-:: Your application ID (must match <id> of Application descriptor) and remove spaces
-set APP_XML=application.xml
-for /f "tokens=3 delims=<>" %%a in ('findstr /R /C:"^[ 	]*<id>" %APP_XML%') do set APP_ID=%%a
-set APP_ID=%APP_ID: =%
+echo %APP_XML%
+echo %APP_ID%
 
 :: find value <description> of Application descriptor and remove spaces
 for /f "tokens=3 delims=<>" %%a in ('findstr /R /C:"^[ 	]*<description>" %APP_XML%') do set string=%%a
 
-::parse json config
-rem Remove quotes
+:: parse json config
+:: Remove quotes
 set string=%string:"=%
-rem Remove braces
+:: Remove braces
 set "string=%string:~2,-2%"
-rem Change colon+space by equal-sign
+:: Change colon+space by equal-sign
 set "string=%string:: ==%"
-rem Separate parts at comma into individual assignments
+:: Separate parts at comma into individual assignments
 set "%string:, =" & set "%"
 
 :menu
@@ -40,13 +37,13 @@ if "%S%"=="1" set SERVER=iran
 if "%S%"=="2" set SERVER=local
 if "%S%"=="3" set SERVER=yoga
 set MARKET=cafebazaar
-cd ..
+
 call bats/SetupDescriptor.bat
 
-REM :clear
+:clear
 
-REM cd %AppData%\%APP_ID%\Local Store\
-REM del /F /Q #SharedObjects\release.swf\%server%-user-data.sol
-REM del /F /Q config.xml
+cd %AppData%\%APP_ID%\Local Store\
+del /F /Q #SharedObjects\release.swf\%server%-user-data.sol
+del /F /Q config.xml
 
-REM cd %~dp0 & cd ..
+cd %~dp0 & cd ..
