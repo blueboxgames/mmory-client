4d718cb91aa0718c1d9ed42323a178712fb7acf2
Mansour <mansurjavadi@gmail.com>
2019-09-18 12:31:09 +0430

    add armv8 and air 33 supprot for google play only

--- bats/SetupSDK.bat
+++ bats/SetupSDK.bat
@@ -5,6 +5,7 @@ cd %~dp0 & cd ..
 
 :: Static path to Flex SDK
 set FLEX_SDK=C:\_projects\AIRSDK_32.0.0.116
+if %MARKET%==google set FLEX_SDK=C:\_projects\AIRSDK_Win_33.0.2.246
 
 :: Use FD supplied SDK path if executed from FD
 if exist "%FD_CUR_SDK%" set FLEX_SDK=%FD_CUR_SDK%
